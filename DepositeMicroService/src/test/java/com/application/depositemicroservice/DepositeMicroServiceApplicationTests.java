dbc47b8365932f40afca8428643948bd31be1969
Samrat Alam <samratalamshanto710@gmail.com>
2024-09-26 19:15:12 +0600

    initial setup

---
+++ DepositeMicroService/src/test/java/com/application/depositemicroservice/DepositeMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.depositemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositeMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
